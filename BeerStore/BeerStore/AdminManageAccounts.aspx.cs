1f24a9a96c1d86c293e55da48095c46414db3761
Wade Carmichael <wade.carmichael@uon.edu.au>
2018-11-09 18:41:12 +1100

    AdminPages
    Signed-off-by: Wade Carmichael <wade.carmichael@uon.edu.au>

--- BeerStore/BeerStore/AdminManageAccounts.aspx.cs
+++ BeerStore/BeerStore/AdminManageAccounts.aspx.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BeerStore.BL;
 
 namespace BeerStore
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        
+        UserAcountBL BL = new UserAcountBL();
         protected void Page_Load(object sender, EventArgs e)
         {
             if(!Request.IsSecureConnection)
@@ -29,5 +32,32 @@ namespace BeerStore
                     TableRowSection.TableHeader;
         }
 
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+            if (e.CommandName.Equals("Delete"))
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow selectedRow = GridView1.Rows[index];
+                string id = selectedRow.Cells[2].Text;
+                BL.deleteUserAccount(Convert.ToInt32(id));
+                Response.Redirect(ConfigurationManager.AppSettings["securePath"] + "AdminManageAccounts.aspx");
+            }
+
+        }
+
+        protected void InsertButton_Click(object sender, EventArgs e)
+        {
+
+            string email = Email.Text;
+            string password = Password.Text;
+            string first = FirstName.Text;
+            string last = LastName.Text;
+            int phone = Convert.ToInt32(PhoneNumber.Text);
+            string address = Address.Text;
+            BL.insertUserAccount(email, password, first, last, phone, address);
+            Response.Redirect(ConfigurationManager.AppSettings["securePath"] + "AdminManageAccounts.aspx");
+        }
     }
 }
--- BeerStore/BeerStore/AdminManageItems.aspx.cs
+++ BeerStore/BeerStore/AdminManageItems.aspx.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BeerStore.BL;
 
 namespace BeerStore
 {
     public partial class AdminManageItems: System.Web.UI.Page
     {
+        UserAccountBL BL = new UserAccountBL();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Request.IsSecureConnection)
@@ -19,12 +21,58 @@ namespace BeerStore
                     "AdminManageItems.aspx";
                 Response.Redirect(url);
             }
+            
+            
+            
         }
 
         public override void VerifyRenderingInServerForm(Control control)
         {
             //
         }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            //finds row from index of the button
+            //uses location of cell to find ID
+            /*if (e.CommandName.Equals("Insert"))
+            {
+                GridViewRow footerRow = GridView1.FooterRow;
+                string name = footerRow.Cells[3].Text;
+                string brand = footerRow.Cells[4].Text;
+                string image = footerRow.Cells[5].Text;
+                decimal price = Convert.ToDecimal(footerRow.Cells[6].Text);
+                string shortDes = footerRow.Cells[7].Text;
+                string longDes = footerRow.Cells[8].Text;
+                BL.insertProduct(name,brand,image,price,shortDes,longDes);
+                //Response.Redirect(ConfigurationManager.AppSettings["securePath"] + "AdminManageItems.aspx");
+            }*/
+            if (e.CommandName.Equals("Delete"))
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow selectedRow = GridView1.Rows[index];
+                string id = selectedRow.Cells[2].Text;
+                BL.deleteProduct(Convert.ToInt32(id));
+                Response.Redirect(ConfigurationManager.AppSettings["securePath"] + "AdminManageItems.aspx");
+            }
+            
+        }
+
+
+        protected void InsertButton_Click(object sender, EventArgs e)
+        {
+            
+            string name = Name.Text;
+            string brand = Brand.Text;
+            string imageString = image.Text;
+            decimal price = Convert.ToDecimal(Price.Text);
+            string shortDes = ShortDescription.Text;
+            string longDes = LongDescription.Text;
+            BL.insertProduct(name, brand, imageString, price, shortDes, longDes);
+            Response.Redirect(ConfigurationManager.AppSettings["securePath"] + "AdminManageItems.aspx");
+        }
+
+
 
 
     }
